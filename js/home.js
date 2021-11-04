e4713497982865c4c3d1b7cb620812c8f2e29171
DiemSuong0412 <19522145@gm.uit.edu.vn>
2021-11-04 22:40:51 +0700

    Update home section

--- js/home.js
+++ js/home.js
@@ -44,9 +44,76 @@ $(document).ready(function(){
           .end().appendTo('.place-card-5 .slide-show'); 
           }, 3000);
     })
+    $(function() {
+      $('.place-card-6 .slide-show img:gt(0)').hide();
+      setInterval(function(){
+        $('.place-card-6 .slide-show :first-child').fadeOut()
+        .next('img').fadeIn() 
+        .end().appendTo('.place-card-6 .slide-show'); 
+        }, 3000);
+    })
+    $(function() {
+      $('.place-card-7 .slide-show img:gt(0)').hide();
+      setInterval(function(){
+        $('.place-card-7 .slide-show :first-child').fadeOut()
+        .next('img').fadeIn() 
+        .end().appendTo('.place-card-7 .slide-show'); 
+        }, 3000);
+    })
+    $(function() {
+      $('.place-card-8 .slide-show img:gt(0)').hide();
+      setInterval(function(){
+        $('.place-card-8 .slide-show :first-child').fadeOut()
+        .next('img').fadeIn() 
+        .end().appendTo('.place-card-8 .slide-show'); 
+        }, 3000);
+    })
+    $(function() {
+      $('.place-card-9 .slide-show img:gt(0)').hide();
+      setInterval(function(){
+        $('.place-card-9 .slide-show :first-child').fadeOut()
+        .next('img').fadeIn() 
+        .end().appendTo('.place-card-9 .slide-show'); 
+        }, 3000);
+    })
+    $(function() {
+      $('.place-card-10 .slide-show img:gt(0)').hide();
+      setInterval(function(){
+        $('.place-card-10 .slide-show :first-child').fadeOut()
+        .next('img').fadeIn() 
+        .end().appendTo('.place-card-10 .slide-show'); 
+        }, 3000);
+    })
 
-    $(document).ready(function(){
-      $('.slider').bxSlider();
-    });
-
+    $('.card-container').slick({
+      isFinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fas fa-angle-left' aria-hidden='true'></i></button>",
+      nextArrow:"<button type='button' class='slick-next pull-right'><i class='fas fa-angle-right' aria-hidden='true'></i></button>",
+      responsive: [
+        {
+          breakpoint: 1276,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            inFinite: true,
+          }
+        },
+        {
+          breakpoint: 900,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 500,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        }
+      ]
+      });
 });
