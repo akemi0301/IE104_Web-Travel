e40afb1fb6a397057072bbfcd0bb43494e060d5c
DiemSuong0412 <19522145@gm.uit.edu.vn>
2021-12-03 20:02:34 +0700

    Add info tour section

--- js/feature-tour.js
+++ js/feature-tour.js
@@ -1,12 +1,34 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+var slideIndex = 1;
+showSlides(slideIndex);
 
-for (let i = 1; i <= 5; i++) {
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-    const newImage = document.createElement("img");
-    newImage.setAttribute("src", "images/tour/tour-tham-quan-banahill-da-nang-" + i + ".jpg");
-    thumbBar.appendChild(newImage);
-    newImage.onclick = function (e) {
-    displayedImage.src = e.target.src;
-    };
-}    
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide-show-feature");
+  var dots = document.getElementsByClassName("more-img");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+function total(){
+      var x= document.getElementsById("slngl").value;
+      var y= document.getElementById("sltre").value;
+      var total = x*875000 + y*500000;
+      document.querySelector('#total').innerHTML = total;
+}
