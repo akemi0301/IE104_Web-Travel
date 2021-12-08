e8b355b1c7dcb5e4578baf714b3779c12d3d5d0f
DiemSuong0412 <19522145@gm.uit.edu.vn>
2021-12-08 16:13:04 +0700

    Update feature section perfect

--- js/feature.js
+++ js/feature.js
@@ -1,29 +1,22 @@
-// const pg = document.querySelector('pagination');
-// const valuePage = {
-//      truncate: true, 
-//      curPage: 1,
-//      numLinksTowSide: 1,
-//      totalPage: 6,
-// }
-// function pagination(){
-//      const{ totalPage, curPage, truncate, numLinksTowSide: delta = valuePage}
+$(document).ready(function() {
+    $("#famous-place").click(function() {
+      $("#list-feature-main").load("famous-place.html");
+    });
 
-// }
-// const range = delta +4;
-// const numberTruncateLeft = curPage - delta;
-// const numberTruncateRight = curPage + delta;
-// let active = '';
-//     for (let pos = 1; pos <= totalPages; pos++) {
-//         active = pos == curPage ? 'active' : '';
-//         if (totalPages >= 2 * range - 1 && truncate) {
-//             if ( numberTruncateLeft > 3 && numberTruncateRight < totalPages - 3 + 1){
-//                 // truncate 2 side
-//                 if (pos >= numberTruncateLeft && pos <= numberTruncateRight) {
-//                   renderTwoSide += renderPage(pos, active);
-//                 }
-//             } 
-//         } else {
-//             // not truncate
-//             render += renderPage(pos, active);
-//         }
-//     }
+    $("#tour-tham-quan").click(function() {
+        $("#list-feature-main").load("tour-tham-quan.html");
+      });
+
+    $("#tour-kham-pha").click(function() {
+      $("#list-feature-main").load("tour-kham-pha.html");
+    });
+
+    $("#tour-du-lich-sinh-thai").click(function() {
+      $("#list-feature-main").load("tour-du-lich-sinh-thai.html");
+    });
+
+    $("#tour-du-lich-van-hoa").click(function() {
+        $("#list-feature-main").load("tour-du-lich-van-hoa.html");
+      });
+
+  });
